2911d24abb5a702bce11cb4433c257ef657092ee
Renzo Carpio <me@renzocarpio.com>
2023-02-13 09:18:56 -0500

    circular progress

--- src/components/PomodoroTime.vue
+++ src/components/PomodoroTime.vue
@@ -1,22 +1,40 @@
 <template>
-  <h1>{{ timeFormat }}</h1>
-  <button @click="initCountDown">Iniciar</button>
-  <button @click="reloadCountDown">Reiniciar</button>
+  <div class="countdown">
+      <div class="time">{{ timeFormat }}</div>
+      <circle-progress :percent="timePercent" />
+  </div>
+  <div class="botones">
+      <button @click="initCountDown">Iniciar</button>
+      <button @click="reloadCountDown">Reiniciar</button>
+  </div>
 </template>
 
 <script>
+import "vue3-circle-progress/dist/circle-progress.css";
+import CircleProgress from "vue3-circle-progress";
 import usePomodoro from '@/composables/usePomodoro';
 
 export default {
     name: 'PomodoroTime',
+    components: {
+        CircleProgress
+    },
     setup(){
-        const { timeMinutes, startCountDown, timeFormat, initCountDown, reloadCountDown } = usePomodoro()
+        const { 
+            timeMinutes,
+            startCountDown, 
+            timeFormat,
+            timePercent,
+            initCountDown,
+            reloadCountDown 
+        } = usePomodoro()
         
 
         return { 
             timeMinutes, 
             startCountDown, 
             timeFormat,
+            timePercent,
             initCountDown,
             reloadCountDown
         }
@@ -25,9 +43,32 @@ export default {
 </script>
 
 <style scoped>
-h1{
-    font-size: 100px;
-    color: green;
+.countdown{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+}
+.botones{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+.time{
+    left:0;
+    right:0;
+    top:0;
+    bottom:0;
+    position:absolute;
+    z-index: 20;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 45px;
+    font-weight: bold;
+    color: #288feb;    
 }
 button{
     color: white;
@@ -35,6 +76,6 @@ button{
     font-size: 30px;
     padding:10px 20px;
     cursor:pointer;
-    margin-right:10px;
+    margin-top:20px;
 }
 </style>
